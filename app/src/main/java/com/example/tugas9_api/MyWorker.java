82d165a60689d01a467ca5e5556b4f499a80a012
Whhhyou <whhhyou26@gmail.com>
2022-12-13 09:32:13 +0700

    Rest API

---
+++ app/src/main/java/com/example/tugas9_api/MyWorker.java
@@ -0,0 +1,37 @@
+package com.example.tugas9_api;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWorker extends Worker {
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+    @NonNull
+    @Override
+    public Result doWork() {
+        displayNotification("Toko Sepeda", "Berhasil Memesan");
+        return Result.success();
+    }
+    public void displayNotification(String task, String desc) {
+        NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new NotificationChannel("Toko Sepeda", "Berhasil Memesan", NotificationManager.IMPORTANCE_HIGH);
+        manager.createNotificationChannel(channel);
+        NotificationCompat.Builder builder = new
+
+                NotificationCompat.Builder(getApplicationContext(),
+                "Toko Sepeda")
+                .setContentTitle(task)
+                .setContentText(desc)
+                .setSmallIcon(R.drawable.ic_logo1)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+        manager.notify(1, builder.build());
+    }
+}
